42ea3f4b6f04a42dc59ea0a9895c55bfec80d328
Estefany Patricia <phaty6815@gmail.com>
2023-01-17 21:47:10 -0500

    servicios

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -49,7 +49,7 @@ export class ProductComponent implements OnInit {
       images:["https://https://i.pinimg.com/originals/15/c5/69/15c56972ae4fbb6b394a6e6efced477b.jpg"],
       categoryId:1
     }
-    const url = "https://api.escuelajs.co/api/v1/products/208";
+    const url = "https://api.escuelajs.co/api/v1/products/192";
     this.httpClient.put(url, data).subscribe(
       response => {console.log(response);
       }
@@ -57,7 +57,7 @@ export class ProductComponent implements OnInit {
   }
 
   deleteProduct(){
-    const url = "https://api.escuelajs.co/api/v1/products/208";
+    const url = "https://api.escuelajs.co/api/v1/products/192";
     this.httpClient.delete(url).subscribe(
       response => {console.log(response);
       }
