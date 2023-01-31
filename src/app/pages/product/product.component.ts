8e3c9635f8e9b9e2019ef4ce68da885bd5c99972
Estefany Patricia <phaty6815@gmail.com>
2023-01-30 20:48:51 -0500

    front y edits

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -3,6 +3,7 @@ import {HttpClient as HttpClient} from '@angular/common/http';
 import { ProductHttpServiceService } from 'src/app/services/product-http-service.service';
 import { CategoriesModel } from 'src/app/entities/categories.model';
 import { CategoryHttpServiceService } from 'src/app/services/category-http-service.service';
+import { ProductModel, UpdateProductDto } from 'src/app/entities/product.model';
 
 @Component({
   selector: 'app-product',
@@ -11,62 +12,71 @@ import { CategoryHttpServiceService } from 'src/app/services/category-http-servi
 })
 export class ProductComponent implements OnInit {
   httpClient: any;
+  products: ProductModel[] = [];
+  selectedProduct: UpdateProductDto={}
   constructor(private productHttpService: ProductHttpServiceService ,
               private categoryHttpServices: CategoryHttpServiceService
     ) {};
-
-
-  getProducts():void{
-    const url ="https://api.escuelajs.co/api/v1/products";
-    this.productHttpService.getAll().subscribe
-    (response => {console.log(response);
-    });
-  }
-
-  getProduct(){
-    const url ="https://api.escuelajs.co/api/v1/products/20";
-    this.productHttpService.getOne(2).subscribe
-    (response => {console.log(response);
-    });
-  }
-
-  createProduct(){
-    const data = {
-      title:"Salas",
-      price:20,
-      description:"Sala en color Beigi / Estefany patricia",
-      images:["https://sdfsdgsdhasjh"],
-      categoryId:1
+    initProduct(){
+      this.selectedProduct = {title:'',price:0,description:''}
     }
-    const url = "https://i.pinimg.com/originals/15/c5/69/15c56972ae4fbb6b394a6e6efced477b.jpg";
-    this.productHttpService.store(data).subscribe(
-      response => {console.log(response);
-      }
-    );
-  }
 
-  updateProduct(){
-    const data = {
-      title:"De la Madera Muebles",
-      price:25,
-      description:"Sala en color Beigi / Estefany Patricia",
-      images:["https://https://i.pinimg.com/originals/15/c5/69/15c56972ae4fbb6b394a6e6efced477b.jpg"],
-      categoryId:1
+
+    getProducts(){
+      this.productHttpService.getAll().subscribe
+      (response => {
+        this.products=response;
+        console.log(response);
+      });
     }
-    const url = "https://api.escuelajs.co/api/v1/products/192";
-    this.productHttpService.update(30, data).subscribe(
-      (response ) => {console.log(response);
-      }
-    );
-  }
 
-  deleteProduct(){
-    const url = "https://api.escuelajs.co/api/v1/products/192";
-    this.productHttpService.destroy(61).subscribe
-    (response => {console.log(response);
+    getProduct(){
+      this.productHttpService.getOne(2).subscribe
+      (response => {console.log(response);
+      });
+    }
+
+
+    createProduct(){
+      const data = {
+        title:"Salas",
+        price:20,
+        description:"Salas color beigi / Estefany Patricia",
+        images:["https://sdfsdgsdhasjh"],
+        categoryId:1
       }
-    );
-  }
+      const url = "https://i.pinimg.com/originals/15/c5/69/15c56972ae4fbb6b394a6e6efced477b.jpg";
+      this.productHttpService.store(data).subscribe(
+        response => {console.log(response);
+        }
+      );
+    }
+
+    updateProduct(){
+      const data = {
+        title:"Salan",
+        price:25,
+        description:"De la madera Muebles/ Estefany Patricia",
+      }
+      const url = "https://api.escuelajs.co/api/v1/products/208";
+      this.productHttpService.update(61, data).subscribe(
+        response => {console.log(response);
+        }
+      );
+    }
+
+    editProduct(product:ProductModel){
+      this.selectedProduct = product;
+    }
+
+    deleteProduct(id:ProductModel['id']) {
+      this.productHttpService.destroy(id).subscribe(
+        response => {
+          this.products=this.products.filter(product => product.id != id);
+          console.log(response);
+        }
+      );
+    }
 
   //////////////////////////////////////////CATEGORIES////////////////////////////////////////////////
 
@@ -113,7 +123,7 @@ export class ProductComponent implements OnInit {
 
 
   ngOnInit(): void{
-    //this.getProducts();
+    this.getProducts();
     //this.getProduct();
     //this.createProduct();
     //this.updateProduct();
